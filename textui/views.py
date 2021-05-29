f54b2eb092fe50cd9b91ba448768fbb3eb2da311
FL45H404 <vipulwaghamode194@gmail.com>
2021-05-29 23:24:05 +0530

    added uppercase service

--- textui/views.py
+++ textui/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 def index(request):
-    data='vipul'
-    return render(request,'index.html',{'data':'vipul'})
-def about(request):
-    return HttpResponse('about')
+    return render(request,'index.html')
+def analyze(request):
+    djtext=request.GET.get('text','default')
+    uppercase=request.GET.get('uppercase','off')
+    removepunc=request.GET.get('removepunc','off')
+    result=''
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        result = ""
+        for char in djtext:
+            if char not in punctuations:
+                result = result + char
+        params ={'res':'result','result':result}
+        djtext = result
+    if(uppercase=="on"):
+        result=djtext.upper()
+        params={'res':'result','result':result}
+    if(removepunc!='on' and uppercase!='on'):
+        return HttpResponse('please select at least one service')
+    return render(request,'index.html',params)
+    
